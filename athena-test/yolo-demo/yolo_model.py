1837edab6b6325f190d468cda52fb269b09a7a7f
panweiji <406798106@qq.com>
2024-05-17 18:01:14 +0800

    chore: 人工智能monorepo单体式仓库

--- athena-test/yolo-demo/yolo_model.py
+++ athena-test/yolo-demo/yolo_model.py
@@ -20,13 +20,13 @@ model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 results = model(["images/cat.jpg"])  # predict on an image
 
 # Process results list
-# for result in results:
-#     boxes = result.boxes  # Boxes object for bounding box outputs
-#     masks = result.masks  # Masks object for segmentation masks outputs
-#     keypoints = result.keypoints  # Keypoints object for pose outputs
-#     probs = result.probs  # Probs object for classification outputs
-#     obb = result.obb  # Oriented boxes object for OBB outputs
-#     result.show()  # display to screen
-#     result.save(filename='result.jpg')  # save to disk
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    result.save(filename='result.jpg')  # save to disk
 
 #path = model.export(format="onnx")  # export the model to ONNX format

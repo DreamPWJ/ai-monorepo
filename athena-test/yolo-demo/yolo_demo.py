55af4f2e05f9107275d5a5d861e3df27e5180573
panweiji <406798106@qq.com>
2024-05-17 15:39:27 +0800

    chore: 人工智能monorepo单体式仓库

--- athena-test/yolo-demo/yolo_demo.py
+++ athena-test/yolo-demo/yolo_demo.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 
-from ultralytics import YOLO
-
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
@@ -9,8 +7,5 @@ model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 # Use the model
 model.train(data="coco8.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("bus.jpg")  # predict on an image
-path = model.export(format="onnx")  # export the model to ONNX format
-print("========================= 目标识别结果 ==============================")
-print(results)
-print("===================================================================")
+results = model("images/cat.png")  # predict on an image
+#path = model.export(format="onnx")  # export the model to ONNX format
